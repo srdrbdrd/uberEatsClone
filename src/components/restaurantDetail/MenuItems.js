20e1b2bf8dfd5b3ab31993b3af3abc1b1547b7d5
srdrbdrd <serdarbedir97@hotmail.com>
2022-01-02 03:36:24 +0300

    MenuItem finished with static data

---
+++ src/components/restaurantDetail/MenuItems.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { View, Text, Image, ScrollView } from 'react-native'
+
+
+
+const foods = [
+    {
+        title: 'Handeden sofraya',
+        description: 'Ev yemekleri',
+        price: '4',
+        image: 'https://www.aquamallavm.com/resimler/hizmet/11557838921.47816.jpg'
+
+    },
+    {
+        title: 'Handeden sofraya',
+        description: 'Ev yemekleri',
+        price: '4',
+        image: 'https://www.aquamallavm.com/resimler/hizmet/11557838921.47816.jpg'
+
+    },
+    {
+        title: 'Handeden sofraya',
+        description: 'Ev yemekleri',
+        price: '4',
+        image: 'https://www.aquamallavm.com/resimler/hizmet/11557838921.47816.jpg'
+
+    },
+    {
+        title: 'Handeden sofraya',
+        description: 'Ev yemekleri',
+        price: '4',
+        image: 'https://www.aquamallavm.com/resimler/hizmet/11557838921.47816.jpg'
+
+    },
+    {
+        title: 'Handeden sofraya',
+        description: 'Ev yemekleri',
+        price: '4',
+        image: 'https://www.aquamallavm.com/resimler/hizmet/11557838921.47816.jpg'
+
+    },
+
+]
+
+
+
+const MenuItems = () => {
+    return (
+        <ScrollView>
+            {foods.map((food, index) => (
+                <View key={index}>
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        margin: 20,
+                    }}>
+                        <FoodInfo food={food} />
+                        <FoodImage food={food} />
+                    </View>
+                    <View style={{ height: 1, backgroundColor: 'grey' }} />
+                </View>))}
+        </ScrollView>
+    );
+}
+
+const FoodInfo = (props) => (
+    <View style={{ width: 240, justifyContent: "space-evenly" }}>
+        <Text style={{ fontSize: 19, fontWeight: '600' }}>{props.food.title}</Text>
+        <Text>{props.food.description}</Text>
+        <Text>{props.food.price}</Text>
+    </View>
+)
+
+const FoodImage = (props) => (
+    <View>
+        <Image source={{ uri: props.food.image }} style={{ width: 100, height: 100, borderRadius: 8 }} />
+    </View>
+)
+
+export default MenuItems
--- src/screens/RestaurantDetail.js
+++ src/screens/RestaurantDetail.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import About from '../components/restaurantDetail/About'
+import MenuItems from '../components/restaurantDetail/MenuItems'
 
 const RestaurantDetail = () => {
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <About />
             <View style={{ height: 1, backgroundColor: 'grey' }} />
+            <MenuItems />
         </View>
     )
 }
